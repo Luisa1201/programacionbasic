8071f867f9a1cdb307ac2a54b3e9483da67f650f
Luisa1201 <lfacostav@ufpso.edu.co>
2025-09-10 11:11:08 -0500

    Ejercicio de Ver password

---
+++ js/function_edad.js
@@ -0,0 +1,45 @@
+function CalcularEdad(){
+    const input = document.getElementById("fecha_nac").value;
+    const resultado = document.getElementById("resultado");
+
+    if (!input){
+        resultado.textContent = "Por favor ingresa una  fecha  de nacimiento"
+        return;
+    }
+    
+    const fecha = new Date(input);
+    const today = new Date();
+    
+    let edad = today.getFullYear() - fecha.getFullYear();
+    let mes = today.getMonth() - fecha.getMonth();
+
+    if(mes < 0 || (mes === 0 && today.getDate() < fecha.getDate())){
+        edad--;
+    }
+
+    resultado.textContent = `Tienes ${edad} años`;
+
+    const date_ejm = new Date();
+
+    console.log(date_ejm);
+    console.log("Día semana " + date_ejm.getDay()); // Inicia en 0
+    console.log("Día Mes " + date_ejm.getDate());
+    console.log("Mes " + date_ejm.getMonth() + 1); // Inicia en 0
+    console.log("Año " + date_ejm.getFullYear());
+    console.log("Hora " + date_ejm.getHours());
+    console.log("Minutos " + date_ejm.getMinutes());
+    console.log("Segundos " + date_ejm.getSeconds());
+    console.log("Milisegundos " + date_ejm.getMilliseconds());
+    
+    const meses = [
+        "Enero", " Febrero", " Marzo", " Abril", 
+        " Mayo", " Junio", " Julio", " Agosto", " Septiembre", 
+        " Octubre", " Noviembre"," Diciembre"
+    ];
+
+    var dias = ["Lunes", " Martes", " Miércoles", " Jueves", " Viernes", " Sábado", " Domingo"];
+    
+    console.log("Día de la semana: " + dias[1]);
+    console.log("Mes del año: " + meses[11]);
+}
+
